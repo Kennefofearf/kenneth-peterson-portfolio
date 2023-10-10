29c58a9b735059c5b969749a5daa85ddab4b0117
Kennefofearf <kpeterson344@gmail.com>
2023-10-10 18:59:57 -0400

    Added data.js and began setting up the Projects section

---
+++ src/components/data.js
@@ -0,0 +1,20 @@
+export const projects = [
+    {
+        title: "COVID-App",
+        description: "An app that pulls confirmed COVID-19 cases vs deaths on the country of theuser's choosing.",
+        image: "",
+        link: "",
+    },
+    {
+        title: "Caddy's Poo Factory",
+        description: "An ecommerce app that sells rabbit manure to farmers or gardners.",
+        image: "",
+        link: "",
+    },
+    {
+        title: "Eatery Shuffle",
+        description: "A restaurant finder app that will randomly choose a nearby for you based on chosen parameters.",
+        image: "",
+        link: "",
+    }
+]
